5ac7290c30c72dab3d95964f74fc95e5e6f02425
gayatri8999 <gayushinde121@gmail.com>
2025-01-30 13:45:24 +0530

    commit my prrojects

---
+++ demo2/src/demo2/variable_declaration.java
@@ -0,0 +1,20 @@
+package demo2;
+
+public class variable_declaration {
+	public static void main(String[] args) {
+		int $num1 =13;
+		int num =24;
+		int FOR =23;
+		long l =13456781;
+		char ch='a';
+		String nameString="shreya";		
+		System.out.println($num1);
+		System.out.println(num);
+		System.out.println(FOR);
+		System.out.println(l);
+		System.out.println(nameString);	
+		System.out.println(ch  );
+		
+	}
+
+}
