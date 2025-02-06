a91d5518e02f58f1ea282846254fbfceeb501e99
gayatri8999 <gayushinde121@gmail.com>
2025-02-06 14:05:26 +0530

    break and continue Statements are done

---
+++ git_repo/src/BreakDemo.java
@@ -0,0 +1,19 @@
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+			
+			for(int i=5; i<10; i++) 
+			{
+				if( i==5)
+				//	break;
+				System.out.println(i);
+				System.out.println("Hello Friends ");
+				break;
+			}
+
+		}
+
+	}
+
+
---
+++ git_repo/src/git_repo/IfelseDemo.java
@@ -0,0 +1,20 @@
+package git_repo;
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+int age=20;
+		
+		if(age> 18)
+		{
+			System.out.println("You are eligible for voting");
+		}
+		else 
+		{
+			System.out.println("Sorry! You need to wait more ");
+		}
+
+
+	}
+
+}
